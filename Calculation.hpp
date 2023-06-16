903e19aef1e1b53923b9884ab83c551be25e3b40
Calculator <laiker19992000@gmail.com>
2023-06-17 02:57:32 +0300

    brackets

--- Calculation.hpp
+++ Calculation.hpp
@@ -3,8 +3,7 @@
 
 #include <string>
 
-enum class Operation
-{
+enum class Operation {
     Addition = 1,
     Subtraction,
     Multiplication,
@@ -12,22 +11,21 @@ enum class Operation
     Invalid
 };
 
-class Calculator
-{
+class Calculator {
 public:
-    void run(); //Главная функция программы, запускающая весь процесс вычислений.
+    void run(); // Главная функция программы, запускающая весь процесс вычислений.
 
 private:
     double first_number_;
     double second_number_;
 
-    void get_two_numbers(); //Запрашивает у пользователя ввод двух чисел.
-    Operation get_operation_choice(); //Запрашивает у пользователя выбор операции.
-    void calculate(const Operation& choice); //Выполняет вычисление в зависимости от выбранной операции.
-    bool ask_for_another_calculation(); //Спрашивает у пользователя, хочет ли он выполнить еще одну операцию.
-    void clear_input(); //Очищает буфер ввода.
-    bool is_exit_symbol(const std::string& input); //Проверяет, является ли введенный символ выходным символом для завершения программы.
-    double get_number_from_input(const std::string& prompt); //Запрашивает у пользователя ввод числа и возвращает его.
+    void get_two_numbers(); // Запрашивает у пользователя ввод двух чисел.
+    Operation get_operation_choice(); // Запрашивает у пользователя выбор операции.
+    void calculate(const Operation& choice); // Выполняет вычисление в зависимости от выбранной операции.
+    bool ask_for_another_calculation(); // Спрашивает у пользователя, хочет ли он выполнить еще одну операцию.
+    void clear_input(); // Очищает буфер ввода.
+    bool is_exit_symbol(const std::string& input); // Проверяет, является ли введенный символ выходным символом для завершения программы.
+    double get_number_from_input(const std::string& prompt); // Запрашивает у пользователя ввод числа и возвращает его.
 };
 
 #endif  // CALCULATOR_HPP
